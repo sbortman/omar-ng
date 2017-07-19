21a171a5511e720b5dd82a550364c9ed1744ee50
melbsurfer <melbsurfer@gmail.com>
2017-07-19 10:10:30 -0400

    Initial commit for OMS Spring Boot app.

---
+++ o2-oms/src/main/groovy/o2/oms/o2oms/O2OmsApplication.groovy
@@ -0,0 +1,12 @@
+package o2.oms.o2oms
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class O2OmsApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run O2OmsApplication, args
+	}
+}
---
+++ o2-oms/src/test/groovy/o2/oms/o2oms/O2OmsApplicationTests.groovy
@@ -0,0 +1,16 @@
+package o2.oms.o2oms
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class O2OmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
